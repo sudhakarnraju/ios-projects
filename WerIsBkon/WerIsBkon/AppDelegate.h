9d4ab94acf2ecbc4ec5e24d22b0556a2ae092958
sudhakarnraju <sudhakarnraju@gmail.com>
2014-03-19 23:41:17 +0530

    initial version

---
+++ WerIsBkon/WerIsBkon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WerIsBkon
+//
+//  Created by sudhakar on 3/19/14.
+//  Copyright (c) 2014 sudhakar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WerIsBkon/WerIsBkonTests/WerIsBkonTests.m
@@ -0,0 +1,34 @@
+//
+//  WerIsBkonTests.m
+//  WerIsBkonTests
+//
+//  Created by sudhakar on 3/19/14.
+//  Copyright (c) 2014 sudhakar. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WerIsBkonTests : XCTestCase
+
+@end
+
+@implementation WerIsBkonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
