eae37ce3f5d5fae1bb57e52acbd950ff9b907ee1
sudhakarnraju <sudhakarnraju@gmail.com>
2014-05-01 00:38:40 +0530

    initial files

---
+++ CouchDBExample/CouchDBExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CouchDBExample
+//
+//  Created by sudhakar on 5/1/14.
+//  Copyright (c) 2014 sudhakar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CouchDBExample/CouchDBExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CouchDBExample
+//
+//  Created by sudhakar on 5/1/14.
+//  Copyright (c) 2014 sudhakar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
